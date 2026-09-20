e0dc7a906ae6accb6783b9a0e57bac074e440227
muhamadabdulshakur250-crypto <muhamadabdulshakur250@gmail.com>
2026-09-20 23:55:25 +0200

    Day1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mybtn').addEventListener('click', function(){
+    document.getElementById('goal').innerText="Goal: Become Full Package Engineer by December and get Remote job!";
+    this.style.display='none';
+})
